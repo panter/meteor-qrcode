7ef653bb8594815b1e021f076ad7eabf40428153
Marco <macrozone@gmail.com>
2015-12-02 10:35:24 +0100

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+# Meteor QR-codes
+
+
+create qr codes in meteor with blaze templates.
+
+Usage:
+
+`{{>qrcode text=url size=1024}}`
--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'panter:qrcode',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Create QR codes in meteor. Kudos to https://larsjung.de/jquery-qrcode/',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/panter/meteor-qrcode.',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
